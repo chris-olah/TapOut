67017a0854fd747dbf493699a1c4f90e07706eeb
chris-olah <colah2123@outlook.com>
2026-02-23 14:50:19 -0500

    Initial Commit

---
+++ TapOut/TapOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapOutApp.swift
+//  TapOut
+//
+//  Created by Chris Olah on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct TapOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
